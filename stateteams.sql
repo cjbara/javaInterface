919612c4f8dbaf1eb97f50eb5e922affe358a227
Cory Jbara <cjbara@users.noreply.github.com>
2016-02-18 17:23:17 -0500

    Delete stateteams.sql

--- stateteams.sql
+++
@@ -1,7 +0,0 @@
-
-select case
-when p.team = 'O' then 'Offense'
-when p.team = 'D' then 'Defense'
-when p.team = 'S' then 'Special Teams' end as team, count(*)
-from roster r, positions p
-where r.position = p.position and r.stcode='IL' group by team;
