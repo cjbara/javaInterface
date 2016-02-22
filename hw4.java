7510973871bef276c4b2f31b1b4b8527cac49f2a
Cory Jbara <cjbara@gmial.com>
2016-02-21 22:31:09 -0500

    Added query 5

--- hw4.java
+++ hw4.java
@@ -105,6 +105,14 @@ class query
                             	System.out.printf("%-20s %-2d\n", rs.getString(1)+", "+rs.getString(2), rs.getInt(3));
                         	}
 	                        break;
+						case 5:
+							q = "select b.city as CITY, b.total as FREQ, sum(case when b.regcode = 1 then b.regcount end) as 'Northeast', sum(case when b.regcode = 2 then b.regcount end) as 'Midwest', sum(case when b.regcode = 3 then b.regcount end) as 'Southeast', ifnull(sum(case when b.regcode = 4 then b.regcount end), 0) as 'Southwest', sum(case when b.regcode = 5 then b.regcount end) as 'West' from ( select a.city, a.total, regcode, count(distinct z.stcode) regcount from ( select city, count(distinct z.stcode) as total from uszips z group by city order by 2 desc, 1 limit 10) a, streg s, uszips z where z.stcode = s.stcode and z.city = a.city group by a.city, regcode order by 2 desc, 1 ) b group by city order by 2 desc, 1;";
+							rs = stmt.executeQuery (q);
+						System.out.printf("%-15s %12s %12s %12s %12s %12s %12s\n", "City", "Total", "Northeast", "Midwest", "Southeast", "Southwest", "West");
+						while(rs.next()){
+							System.out.printf("%-15s %12d %12d %12d %12d %12d %12d\n", rs.getString(1), rs.getInt(2), rs.getInt(3), rs.getInt(4), rs.getInt(5), rs.getInt(6), rs.getInt(7));
+						}
+						break;
                                         default:
                                                 System.out.println("Please enter a valid number");
                                                 break;
